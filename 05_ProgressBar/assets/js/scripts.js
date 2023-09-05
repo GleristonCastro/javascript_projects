a564e3178128a6f6576a28b3462cfdc124e55542
Gleriston <xpanium@gmail.com>
2023-09-05 17:25:25 -0300

    scripts of progress bar project

---
+++ 05_ProgressBar/assets/js/scripts.js
@@ -0,0 +1,21 @@
+const progressBar = document.querySelector('.progress');
+const previousBtn = document.querySelector('#previous-btn');
+const nextBtn = document.querySelector('#next-btn');
+
+let progress = 0;
+
+function updateProgressBar() {
+  progressBar.style.width = progress + '%';
+}
+
+nextBtn.addEventListener('click', () => {
+  progress += 10;
+  if(progress > 100) progress = 100;
+  updateProgressBar();
+});
+
+previousBtn.addEventListener('click', () => {
+  progress -= 10;
+  if(progress < 0) progress = 0;
+  updateProgressBar();
+});
