ee8a866da12875d855ab7f1ff8a6d83a3c0b7492
Gleriston <xpanium@gmail.com>
2023-09-09 15:12:39 -0300

    update scripts Form with validation project

--- 06_FormWithValidation/assets/js/scripts.js
+++ 06_FormWithValidation/assets/js/scripts.js
@@ -1,34 +1,57 @@
 const form = document.querySelector('form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const subjectInput = document.querySelector('#subject');
-const messageInput = document.querySelector('#message');
+const nameUser = document.querySelector('#name');
+const email = document.querySelector('#email');
+const subject = document.querySelector('#subject');
+const message = document.querySelector('#message');
 const errorMessages = document.querySelectorAll('.error-message');
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
+  resetErrors();
   validateInputs();
 });
 
-function setError(input, errorMessage){
+function resetErrors() {
+  errorMessages.forEach((errorMessage) => {
+    errorMessage.innerText = '';
+  });
+  nameUser.parentElement.classList.remove('error');
+  email.parentElement.classList.remove('error');
+  subject.parentElement.classList.remove('error');
+  message.parentElement.classList.remove('error');
+}
+
+function validateInputs() {
+  const nameUserValue = nameUser.value.trim();
+  const emailValue = email.value.trim();
+  const subjectValue = subject.value.trim();
+  const messageValue = message.value.trim();
+
+  if (nameUserValue === '') {
+    setError(nameUser, 'Name cannot be left blank');
+  }
+
+  if (emailValue === '') {
+    setError(email, 'Email cannot be blank');
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, 'Invalid email');
+  }
+
+  if (subjectValue === '') {
+    setError(subject, 'Subject cannot be left blank');
+  }
+
+  if (messageValue === '') {
+    setError(message, 'Message cannot be left blank');
+  }
+}
+
+function setError(input, errorMessage) {
   const errorMessageElement = input.nextElementSibling;
-  errorMessageElement.textContent = errorMessage;
+  errorMessageElement.innerText = errorMessage;
   input.parentElement.classList.add('error');
-};
+}
 
-function validateInputs(){
-  const nameValue = nameInput.value.trim();
-  const emailValue = emailInput.value.trim();
-  const subjectValue = subjectInput.value.trim();
-  const messageValue = messageInput.value.trim();
-
-  if(nameValue === '') setError(nameInput, 'Name cannot be blank');
-
-  emailValue === ''
-  ?  setError(emailInput, 'Email cannot be blank')
-  : setError(emailInput, 'Email invalid');
-
-  if(subjectValue === '') setError(subjectInput, 'Subject cannot be blank');
-
-  if(messageValue === '') setError(messageInput, 'Message cannot be blank');
-};
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
